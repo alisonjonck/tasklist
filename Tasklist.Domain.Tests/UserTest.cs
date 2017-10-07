356c77cbaf438d3286f466ef54e1b957193663d4
Alison Jonck <alison.jonck@totvspartners.com.br>
2017-10-07 02:31:17 -0300

    added first test to user

---
+++ Tasklist.Domain.Tests/UserTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tasklist.Domain.Entity;
+
+namespace Tasklist.Domain.Tests
+{
+	[TestClass]
+	public class UserTest
+	{
+		[TestMethod]
+		public void TestUserExists()
+		{
+			var user = new User();
+			Assert.IsInstanceOfType(user, typeof(User));
+			Assert.IsNotNull(user);
+		}
+	}
+}
